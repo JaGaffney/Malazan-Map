f86a824aaa2a8d07e4126694aad5a3c98c880cd3
JaGaffney <jagaffney661@msn.com>
2024-01-29 13:50:50 +1100

    feat: updated info

--- src/components/panel/Acknowledgements.tsx
+++ src/components/panel/Acknowledgements.tsx
@@ -4,12 +4,16 @@ const Acknowledgements = () => {
     return (
         <div className="panel__item-container">
             <h5>Acknowledgements</h5>
-            <div className="panel__item-container-info panel__item-container-info-small">
+            <div className="panel__item-container-info panel__item-container-info-small" style={{ maxWidth: "15rem" }}>
                 <span>I was finding it hard to keep track of where the characters are in book 2, so I decided to make a map. I am still in the process of reading so no spoilers please.
                 </span>
             </div>
             <a href="https://steven-erikson.org/" className="panel__item-container-info panel__item-container-info-small">
                 <span>Steven Erikson</span>
+                <span>Author of the books</span>
+            </a>
+            <a href="https://ian-esslemont.com/" className="panel__item-container-info panel__item-container-info-small">
+                <span>Ian C. Esslemont</span>
                 <span>Author of the books</span>
             </a>
             <a href="https://atlasoficeandfireblog.wordpress.com/2020/03/04/a-new-almost-definitive-malazan-world-map/" className="panel__item-container-info  panel__item-container-info-small">
@@ -24,6 +28,13 @@ const Acknowledgements = () => {
                 <span>/u/SageOfTheWise</span>
                 <span>Timeline</span>
             </a>
+            <span></span>
+            <a href="https://github.com/JaGaffney" className="panel__item-container-info  panel__item-container-info-small">
+                <span>github@jagaffney</span>
+                <span>Website creator</span>
+            </a>
+
+
         </div>
     )
 }
